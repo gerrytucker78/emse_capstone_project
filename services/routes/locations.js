f8d39dcaf43a35e2223a659738b3f8569040b37c
Gerry Tucker <gerrytucker@gmail.com>
2017-03-24 20:15:41 -0500

    Added paths to be part of the locations interface

--- services/routes/locations.js
+++ services/routes/locations.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var Location = require('../models/locations.js');
+var Path = require('../models/paths.js');
 
 var config = require('config');
 var simulation = config.get('simulation');
@@ -21,6 +22,17 @@ router.get('/', function (req, res, next) {
     } else {
         Location.findAll({order: [['location_id', 'ASC']]}).then(function (locations) {
             return res.send(locations)
+        });
+    }
+});
+
+/* GET ALL location paths listing. */
+router.get('/paths', function (req, res, next) {
+    if (simulation.get("enabled") == "true") {
+        res.send(simDataSet);
+    } else {
+        Path.findAll({order: [['start_id', 'ASC']]}).then(function (paths) {
+            return res.send(paths)
         });
     }
 });
@@ -69,13 +81,39 @@ router.post('/', function (req, res, next) {
 });
 
 /**
- * DELETE Specific location
+ * POST Add Path
+ */
+router.post('/path', function (req, res, next) {
+
+    Path.create({
+        start_id: req.body.start_id,
+        end_id: req.body.end_id,
+        weight: req.body.weight
+    }).then(function (loc) {
+        res.send(loc)
+    });
+});
+
+/**
+ * DELETE Specific location OR All
  */
 router.delete('/', function (req, res, next) {
     if (req.body.location_id === undefined) {
         Location.destroy({truncate: true}).then(res.sendStatus(200));
     } else {
         Location.destroy({where: {location_id: req.body.location_id}}).then(res.sendStatus(200));
+    }
+
+});
+
+/**
+ * DELETE Specific path OR All
+ */
+router.delete('/paths', function (req, res, next) {
+    if (req.body.start_id === undefined && req.body.end_id === undefined) {
+        Path.destroy({truncate: true}).then(res.sendStatus(200));
+    } else {
+        Path.destroy({where: {start_id: req.body.start_id} & {end_id: req.body.end_id}}).then(res.sendStatus(200));
     }
 
 });
