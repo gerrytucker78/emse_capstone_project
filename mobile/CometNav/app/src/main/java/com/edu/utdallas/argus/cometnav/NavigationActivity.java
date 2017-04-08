88f3a8cfcee3ca73cea02678c8e81112027b6bde
Chika Ihejimba <info@chikaihejimba.com>
2017-04-08 11:39:22 -0500

    Map integration.

--- mobile/CometNav/app/src/main/java/com/edu/utdallas/argus/cometnav/NavigationActivity.java
+++ mobile/CometNav/app/src/main/java/com/edu/utdallas/argus/cometnav/NavigationActivity.java
@@ -2,6 +2,7 @@ package com.edu.utdallas.argus.cometnav;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,27 +10,57 @@ import android.widget.ImageView;
 
 
 import java.io.File;
+import java.io.InputStream;
+
+import static android.graphics.BitmapFactory.*;
 
 public class NavigationActivity extends AppCompatActivity
 {
 
-    File file = new File("./mapFile");
-    ImageView image;
+//    File file = new File("./mapFile");
+//    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
-        image = (ImageView) findViewById(R.id.imageView2);
+//        image = (ImageView) findViewById(R.id.imageView2);
+        new DownloadImageTask((ImageView) findViewById(R.id.imageView2))
+                .execute("https://s3-us-west-2.amazonaws.com/got150030/capstone/ECSS2.png");
 
-        DataServices.getMap(this, file);
+//        DataServices.getMap(this, file);
     }
 
     public void updateMap(File map)
     {
         Log.d("NavigationActivity", "Got image callback");
         Bitmap bitmap = BitmapFactory.decodeFile(map.getAbsolutePath());
-        image.setImageBitmap(bitmap);
+//        image.setImageBitmap(bitmap);
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
