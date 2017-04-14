ef86404197c023ad05ceceae381fa781dd627f49
Daniel <dandehnel102@gmail.com>
2017-04-14 15:55:45 -0500

    Adding what I have so far
    Integrate dot with async loading, and resize dot on zoom. However, can't
    figure out how to erase the dot without erasing the entire map!

--- mobile/CometNav/app/src/main/java/com/edu/utdallas/argus/cometnav/NavigationActivity.java
+++ mobile/CometNav/app/src/main/java/com/edu/utdallas/argus/cometnav/NavigationActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +20,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
+import com.github.chrisbanes.photoview.OnMatrixChangedListener;
+import com.github.chrisbanes.photoview.OnPhotoTapListener;
+import com.github.chrisbanes.photoview.OnScaleChangedListener;
 import com.github.chrisbanes.photoview.PhotoView;
 
 import org.altbeacon.beacon.Beacon;
@@ -41,20 +47,28 @@ public class NavigationActivity extends AppCompatActivity
         }
         super.onDestroy();
     }
-    private ImageView img;
+
     private PhotoView photoView;
     private DownloadImageTask task;
-    private LocationPointer pointer;
+
+    private Canvas locDot;
+    private Paint paint;
+    private Bitmap immutableMap;
+    private Bitmap mutableMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+        //mCustomDrawableView = new CustomDrawableView(this);
+        //setContentView(mCustomDrawableView);
+        //setContentView(new MyView(this));
 
-        //photoView = (PhotoView) findViewById(R.id.photo_view);
-        //img = (ImageView) findViewById(R.id.imageView2);
+        photoView = (PhotoView) findViewById(R.id.photo_view);
         task = new DownloadImageTask(photoView);
         task.execute("https://s3-us-west-2.amazonaws.com/got150030/capstone/ECSS2.png");
+
         //TODO Find Beacons on create. Move to only find beacons when ready to navigate
         Intent intent = new Intent(this, BeaconManagerService.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -69,78 +83,80 @@ public class NavigationActivity extends AppCompatActivity
             @Override
             public void onReceive(Context context, Intent intent) {
                 List<Beacon> beaconArrayList = intent.getParcelableArrayListExtra("BEACON_LIST");
-                Log.d("Navigation", "Received beacon broadcast! " +beaconArrayList.toString() );
+               // Log.d("Navigation", "Received beacon broadcast! " +beaconArrayList.toString() );
 
                 //do something based on the intent's action
             }
         };
         registerReceiver(receiver, filter);
 
+        photoView.setAdjustViewBounds(true);
+        photoView.setOnPhotoTapListener(new PhotoTapListener());
+        photoView.setOnScaleChangeListener(new ScaleChangeListener());
+    }
 
+    private class PhotoTapListener implements OnPhotoTapListener {
 
-        /*Draw a circle on an existing image - starts here*/
-        BitmapFactory.Options myOptions = new BitmapFactory.Options();
-        myOptions.inDither = true;
-        myOptions.inScaled = false;
-        myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
-        myOptions.inPurgeable = true;
+        @Override
+        public void onPhotoTap(ImageView view, float x, float y) {
+            float xPercentage = x * 100f;
+            float yPercentage = y * 100f;
 
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ecss,myOptions);
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setColor(Color.BLUE);
+            String PHOTO_TAP_TOAST_STRING = "Photo Tap! X: %.2f %% Y:%.2f %% ID: %d";
+            Log.d("Navigation", (String.format(PHOTO_TAP_TOAST_STRING, xPercentage, yPercentage, view == null ? 0 : view.getId())));
+        }
+    }
 
+    private float cumulScaleFactor = 1;
+    private int mX = 200;
+    private int mY = 250;
+    private class ScaleChangeListener implements OnScaleChangedListener {
 
-        Bitmap workingBitmap = Bitmap.createBitmap(bitmap);
-        Bitmap mutableBitmap = workingBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        public void onScaleChange(float scaleFactor, float focusX, float focusY) {
+            cumulScaleFactor = cumulScaleFactor * scaleFactor;
+            //Log.d("Navigation", "Scale changed: " + cumulScaleFactor);
 
+            locDot.save();
+            //TODO! This clears the entire map! need a way to not do that
+            //locDot.drawColor(0, PorterDuff.Mode.CLEAR);
 
-        Canvas canvas = new Canvas(mutableBitmap);
-        canvas.drawCircle(200, 250, 10, paint);
-
-
-        ImageView imageView = (ImageView)findViewById(R.id.photo_view);
-        imageView.setAdjustViewBounds(true);
-        imageView.setImageBitmap(mutableBitmap);
-
-        /*Draw a circle on an existing image - ends here*/
-
-
-//        DataServices.getMap(this, file);
-        //setContentView(new MyView(this)); //TODO Remove this reference "MyView"
-        //img=(ImageView) findViewById(R.id.locator);
-
-
-        //Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_locate);
-        //pointer = new LocationPointer(icon, img);
-        //new LocationPointer(icon,img);
+            locDot.translate(mX+=10, mY);
+            float scaleVal = (1/cumulScaleFactor);
+            Log.d("Navigation", "scaleVal: " + scaleVal);
+            locDot.scale(scaleVal, scaleVal);
+            locDot.drawCircle(0, 0, 10, paint);
+            locDot.restore();
+        }
     }
 
     //TODO Remove this class "MyView"
     public class MyView extends View
     {
-        Paint paint = null;
+        private Paint   mBitmapPaint;
+        private Bitmap  mBitmap;
+        private Canvas  mCanvas;
+
         public MyView(Context context)
         {
             super(context);
-            paint = new Paint();
+            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+            BitmapFactory.Options myOptions = new BitmapFactory.Options();
+            myOptions.inDither = true;
+            myOptions.inScaled = false;
+            myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
+            myOptions.inPurgeable = true;
+            mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ecss,myOptions);
         }
 
         @Override
         protected void onDraw(Canvas canvas)
         {
-            super.onDraw(canvas);
-            int x = getWidth();
-            int y = getHeight();
-            int radius;
-            radius = 100;
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.WHITE);
-            canvas.drawPaint(paint);
-            // Use Color.parseColor to define HTML colors
-            paint.setColor(Color.parseColor("#CD5C5C"));
-            canvas.drawCircle(x / 2, y / 2, radius, paint);
+            //canvas.drawColor(0xFFAAAAAA);
+            canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+            canvas.drawCircle(200, 250, 10, mBitmapPaint);
+            //canvas.drawPath(mPath, mPaint);
         }
+
     }
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
@@ -170,8 +186,26 @@ public class NavigationActivity extends AppCompatActivity
 
         protected void onPostExecute(Bitmap result) {
             if (bmImage != null) {
-                bmImage.setImageBitmap(result);
 
+                 /*Draw a circle on an existing image - starts here*/
+                BitmapFactory.Options myOptions = new BitmapFactory.Options();
+                myOptions.inDither = true;
+                myOptions.inScaled = false;
+                myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
+                myOptions.inPurgeable = true;
+
+                paint = new Paint();
+                paint.setAntiAlias(true);
+                paint.setColor(Color.BLUE);
+
+                //Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.id.image_view,myOptions);
+                immutableMap = result;
+                mutableMap = immutableMap.copy(Bitmap.Config.ARGB_8888, true);
+
+                locDot = new Canvas(mutableMap);
+                locDot.drawCircle(200, 250, 10, paint);
+
+                bmImage.setImageBitmap(mutableMap);
             }
             else
                 Log.d("Navigation", "bmImage is null?");
