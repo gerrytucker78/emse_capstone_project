b14493c1cf1077d44c32b2de0c72b39f91e274cd
Chika Ihejimba <info@chikaihejimba.com>
2017-04-15 13:35:42 -0500

    Removed Google API placeholder

--- mobile/CometNav/app/src/main/java/com/edu/utdallas/argus/cometnav/FindARoomMapsActivity.java
+++ mobile/CometNav/app/src/main/java/com/edu/utdallas/argus/cometnav/FindARoomMapsActivity.java
@@ -3,45 +3,18 @@ package com.edu.utdallas.argus.cometnav;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FindARoomMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class FindARoomMapsActivity extends FragmentActivity {
 
-    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_aroom_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(this);*/
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker at Erik Jonsson.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Erik Jonsson and move the camera
-        LatLng ErikJonsson = new LatLng(32.9863479,-96.7503571);
-        mMap.addMarker(new MarkerOptions().position(ErikJonsson).title("Erik Jonsson SEC"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ErikJonsson));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ErikJonsson,18));
-    }
 }
