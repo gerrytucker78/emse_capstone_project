b0e4955e33fd26cd6b5800bc90c10a72125e7a03
Gerry Tucker <gerrytucker@gmail.com>
2017-04-17 00:08:07 -0500

    Added support for pulling from backend service

--- services/controllers/maintainLocations.js
+++ services/controllers/maintainLocations.js
@@ -5,31 +5,73 @@ var showPaths = true;
 var exits = true;
 var stairs = true;
 var labels = true;
+var floor = 2;
+
+var locations = {};
+var paths = [];
+var sensors = [];
 
 
 var maintainLocations = angular.module('maintainLocations', ['ngSanitize']);
-maintainLocations.controller('locationController', ['$scope','$http', function($scope,$http) {
+maintainLocations.controller('locationController', ['$scope', '$http', function ($scope, $http) {
     $scope.message = 'Initial Load';
 
-    $scope.togglePaths = function() {
+    $scope.togglePaths = function () {
         showPaths = !showPaths;
+        $scope.message = 'Toggling paths: ' + showPaths
+
         drawData();
     };
 
-    $scope.toggleBeacons = function() {
+    $scope.toggleBeacons = function () {
         beacons = !beacons;
         drawData();
     };
 
-    $scope.toggleHalls = function() {
+    $scope.toggleHalls = function () {
         halls = !halls;
         drawData();
     };
 
-    $scope.toggleRooms = function() {
+    $scope.toggleHalls = function () {
+        labels = !labels;
+        drawData();
+    };
+
+    $scope.toggleRooms = function () {
         rooms = !rooms;
         drawData();
     };
+
+    $scope.loadData = function () {
+        $scope.message = "Loading /locations"
+        $http.get('/locations').success(function (data, status, headers, config) {
+            $scope.message = "Results" + data;
+
+            for (i = 0; i < data.length; i++) {
+                locations[data[i].location_id] = data[i];
+            }
+
+
+            $http.get('/locations/paths').success(function (data, status, headers, config) {
+                paths = data;
+                $scope.message = "Paths Results" + data;
+
+                $http.get('/sensors').success(function (data, status, headers, config) {
+                    sensors = data;
+                    drawData();
+                }).error(function (data, status, headers, config) {
+                    // TO-DO: Need to fill in.
+                });
+            }).error(function (data, status, headers, config) {
+                // TO-DO: Need to fill in.
+            });
+
+        }).error(function (data, status, headers, config) {
+            // TO-DO: Need to fill in.
+        });
+
+    }
 
 }]);
 
@@ -40,38 +82,57 @@ function drawData() {
     ctx.drawImage(img, 0, 0);
 
     var markerSize = 5;
-    var markerOffset = markerSize/2;
+    var markerOffset = markerSize / 2;
 
-    var c=document.getElementById("myCanvas");
-    var ctx=c.getContext("2d");
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
 
-    for (var i = 0; i < locations.length; i++) {
+    for (var i in locations) {
         var loc = locations[i];
-        ctx.font="10px Arial";
+        ctx.font = "10px Arial";
 
+        if (locations[i].floor == floor) {
+            ctx.fillStyle = "#000000"
+            if ((locations[i].type == "BEACON" && beacons) || (locations[i].type == "HALL" && halls) || (locations[i].type == "ROOM" && rooms) || (locations[i].type == "EXIT" && exits) || (locations[i].type == "STAIRS" && stairs)) {
+                if (labels) {
+                    ctx.fillText(i, locations[i].pixel_loc_x - markerOffset - 10, locations[i].pixel_loc_y + 10);
+                }
 
-        if ((locations[i].type == "BEACON" && beacons) || (locations[i].type == "HALL" && halls) || (locations[i].type == "ROOM" && rooms) || (locations[i].type == "EXIT" && exits) || (locations[i].type == "STAIRS" && stairs)) {
-            if (labels) {
-                ctx.fillText(i,locations[i].pixel_loc_x-markerOffset-10,locations[i].pixel_loc_y+10);
+                if (locations[i].type == "BEACON") {
+                    ctx.fillStyle = "#FF0000"
+                } else {
+                    ctx.fillStyle = "#000000"
+                }
+
+                ctx.fillRect((loc.pixel_loc_x + markerOffset), (loc.pixel_loc_y + markerOffset), 5, 5);
             }
-
-            if (locations[i].type == "BEACON") {
-                ctx.fillStyle="#FF0000"
-            } else {
-                ctx.fillStyle="#000000"
-            }
-
-            ctx.fillRect((loc.pixel_loc_x + markerOffset),(loc.pixel_loc_y + markerOffset),5,5);
         }
     }
+
+    for (var i = 0; i < sensors.length; i++) {
+        var loc = sensors[i];
+        ctx.font = "10px Arial";
+
+        if (sensors[i].floor == floor) {
+            if (beacons) {
+
+                ctx.fillStyle = "#FF0000"
+                if (labels) {
+                    ctx.fillText(i, sensors[i].pixel_loc_x - markerOffset - 10, sensors[i].pixel_loc_y + 10);
+                }
+                ctx.fillRect((loc.pixel_loc_x + markerOffset), (loc.pixel_loc_y + markerOffset), 5, 5);
+            }
+        }
+    }
+
 
     for (var i = 0; i < paths.length; i++) {
         var cPath = paths[i];
 
-        if (showPaths) {
+        if (showPaths && locations[cPath.start_id].floor == floor) {
             ctx.beginPath();
-            ctx.moveTo(locations[cPath.start_id].pixel_loc_x+markerSize,locations[cPath.start_id].pixel_loc_y+markerSize);
-            ctx.lineTo(locations[cPath.end_id].pixel_loc_x+markerSize,locations[cPath.end_id].pixel_loc_y+markerSize);
+            ctx.moveTo(locations[cPath.start_id].pixel_loc_x + markerSize, locations[cPath.start_id].pixel_loc_y + markerSize);
+            ctx.lineTo(locations[cPath.end_id].pixel_loc_x + markerSize, locations[cPath.end_id].pixel_loc_y + markerSize);
             ctx.stroke();
         }
     }
