48a814a9e3e88c3d8d21497464bed1a2362244a8
Chika Ihejimba <info@chikaihejimba.com>
2017-04-23 16:09:43 -0500

    1. Updated the hamburger link on the homepage to point to the new Navigate_To Page 2. Updated the look and feel of the Navigate_To Page

--- mobile/CometNav/app/src/main/java/com/edu/utdallas/argus/cometnav/MainActivity.java
+++ mobile/CometNav/app/src/main/java/com/edu/utdallas/argus/cometnav/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
-                startActivity(new Intent(MainActivity.this, FindARoomMapsActivity.class));
+                startActivity(new Intent(MainActivity.this, Navigate_To.class));
 
 
             }
